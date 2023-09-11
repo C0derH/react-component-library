3da7974f28ca940348c3b8b8eb0a7ab81e2efa91
CoderH <legendaryh1381@gmail.com>
2023-09-11 09:55:19 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+import Badge from "./components/Badge"
+
+function App() {
+
+
+  return (
+    <>
+      <Badge>
+        Badge
+      </Badge>
+    </>
+  )
+}
+
+export default App
